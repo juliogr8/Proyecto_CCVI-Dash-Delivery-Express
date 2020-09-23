218590fdf8f66ed6a52562a4ff0f33cbadb54caa
Julio Gudiel <18001201@galileo.edu>
2020-09-22 23:45:51 -0600

    ahora sí

--- src/app/components/envio/envio.component.ts
+++ src/app/components/envio/envio.component.ts
@@ -19,21 +19,21 @@ export class EnvioComponent implements OnInit {
 
   ciudad = {
     "01001": "Guatemala",
-    "02001": "El Progreso",
-    "03001": "Sacatepequez",
+    "02001": "Guastatoya",
+    "03001": "Antigua Guatemala",
     "04001": "Chimaltenango",
     "05001": "Escuintla",
-    "06001": "Santa Rosa",
+    "06001": "Cuilapa",
     "07001": "Sololá",
     "08001": "Totonicapán",
     "09001": "Quetzaltenango",
-    "10001": "Suchitepequez",
+    "10001": "Mazatenango",
     "11001": "Retalhuleu",
     "12001": "San Marcos",
     "13001": "Huehuetenango",
-    "14001": "Quiché",
-    "15001": "Baja Verapaz",
-    "16001": "Alta Verapaz",
+    "14001": "Santa Cruz Quiché",
+    "15001": "Salamá",
+    "16001": "Cobán",
     "17001": "Petén",
     "18001": "Izabal",
     "19001": "Zacapa",
@@ -67,11 +67,10 @@ export class EnvioComponent implements OnInit {
     });
       this.ordenesServicio.getNo_Orden().subscribe(result => {
         this.no_orden = "Su número de orden es: " + result[0].last_value
+        this.costo = "Costo: " + result[0].costo
         console.log(result)
         console.log(this.no_orden)
       }
         );
-        if(this.tipo == "Sobres") { this.costo = "Costo de envío: Q30.00" }
-        else { this.costo="Costo de envío: Q30.00" }
   }
 }
